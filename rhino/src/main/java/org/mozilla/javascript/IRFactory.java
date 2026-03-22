7f9411344c1409236900caf412dfcc1e03e0fd46
ms900 <ms900@users.noreply.github.com>
2026-03-23 05:46:38 +0800

    fix: restore ES6 version check for __proto__ property access
    ES6 mode should use normal property access for __proto__, not SpecialRef.
    This was incorrectly removed in a previous commit.

--- rhino/src/main/java/org/mozilla/javascript/IRFactory.java
+++ rhino/src/main/java/org/mozilla/javascript/IRFactory.java
@@ -2253,23 +2253,8 @@ public final class IRFactory {
                 return parser.createName(name);
             }
             parser.checkActivationName(name, Token.GETPROP);
-            if (ScriptRuntime.isSpecialProperty(name)) {
-                if (target.getType() == Token.SUPER) {
-                    // We have an access to super.__proto__ or super.__parent__.
-                    // This needs to behave in the same way as this.__proto__ - it really is not
-                    // obvious why, but you can test it in v8 or any other engine. So, we just
-                    // replace SUPER with THIS in the AST. It's a bit hacky, but it works - see the
-                    // test cases in SuperTest!
-                    if (!(target instanceof KeywordLiteral)) {
-                        throw Kit.codeBug();
-                    }
-                    KeywordLiteral oldTarget = (KeywordLiteral) target;
-                    target =
-                            new KeywordLiteral(
-                                    oldTarget.getPosition(), oldTarget.getLength(), Token.THIS);
-                    target.setLineColumnNumber(oldTarget.getLineno(), oldTarget.getColumn());
-                }
-
+            if (parser.compilerEnv.getLanguageVersion() < Context.VERSION_ES6
+                    && ScriptRuntime.isSpecialProperty(name)) {
                 Node ref = new Node(Token.REF_SPECIAL, target);
                 ref.putProp(Node.NAME_PROP, name);
                 Node getRef = new Node(Token.GET_REF, ref);
